7fd7b5f3a700d69a2d8287ede6df4d55a0f1e658
gerson aguiar <gersoncafilho@gmail.com>
2024-07-21 11:02:38 -0300

    eslint - But the configuration is not workin. I need to update to the current version config.

--- index.js
+++
@@ -1,8 +0,0 @@
-// function calculateAgeOfUser(user) {
-//   return new Date().getFullYear() - user.birthYear;
-// }
-
-// calculateAgeOfUser('eali');
-
-// // Runtime Type Checking
-// // Static Type Checking
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+app.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+  }
+
+   console.log(`Server listening at - v0.0.1 - ${address}`);
+})
+
+
+// "editor.codeActionsOnSave": {
+//     "source.fixAll.eslint": "explicit"
+//   },
